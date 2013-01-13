845e6318f88e626ccf8056d5d66dd49981aaa3e6
Luis Sergio Curay Benites <metro_rex@hotmail.com>
2013-01-13 13:03:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UbuntuApp::Application.config.secret_token = 'c9bd32aa3ba70d60c51aa2a8cbffd6bdc8d876430e5443a29760132cf176c22c8ade780c82584a68df9f7185d5be81af88ace9e53286382c5ce445501fb419a2'
